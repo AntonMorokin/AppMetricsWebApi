300273c6c179fe79f8f103fef7f2f25b43d09d11
Anton Morokin <anton_morokin@gmail.com>
2021-12-29 21:15:16 +0300

    Initial commit.

---
+++ AppMetricsWepApi/Controllers/ClientController.cs
@@ -0,0 +1,38 @@
+﻿using AppMetricsWepApi.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppMetricsWepApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public sealed class ClientsController : ControllerBase
+    {
+        [HttpGet("{clientId}")]
+        public async Task<IActionResult> Get(int clientId)
+        {
+            await Task.Delay(new Random().Next(100) + 100);
+
+            return new OkResult();
+        }
+
+        [HttpPost]
+        public Task<IActionResult> Create()
+        {
+            return Task.FromResult<IActionResult>(new CreatedResult("api/cleints/create", null));
+        }
+
+        [HttpPatch("modify")]
+        public async Task<IActionResult> Patch(Client changedClient)
+        {
+            await Task.Delay((new Random().Next(10) + 1) * 1000);
+
+            return new OkResult();
+        }
+
+        [HttpDelete("remove/{clientId}")]
+        public Task<IActionResult> Delete(int clientId)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ AppMetricsWepApi/Model/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace AppMetricsWepApi.Model
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+    }
+}
